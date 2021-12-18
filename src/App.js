0aa93e9aff09428781a4e6d927fd945c0370d2a4
Zhi Yong Han <hanzyong0@gmail.com>
2021-12-18 21:56:26 +0800

    add General component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React, { Component } from 'react'
+import General from './components/General'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: '',
+      email: '',
+      phone: '',
+    }
+  }
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    console.log(this.state)
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  };
+
+  render() {
+    const { name, email, phone } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.onSubmitForm}>
+          <General
+            name={name}
+            email={email}
+            phone={phone}
+            onInputChange={this.handleChange}
+          />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
---
+++ src/components/General.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+class General extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { name, email, phone, onInputChange } = this.props;
+    return (
+      <div>
+        <div>
+          <label htmlFor='name'>Name: </label>
+          <input
+            type='text'
+            id='name'
+            value={name}
+            onChange={onInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor='email'>Email: </label>
+          <input
+            type='email'
+            id='email'
+            value={email}
+            onChange={onInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor='phone'>Phone Number: </label>
+          <input
+            type='tel'
+            id='phone'
+            value={phone}
+            onChange={onInputChange}
+          />
+        </div>
+        <p>{name} {email} {phone}</p>
+      </div>
+    );
+  };
+};
+
+export default General;
